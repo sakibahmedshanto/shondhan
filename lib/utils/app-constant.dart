b0249ebf3545b6ee0e71cd1caf844d8af807db07
sakibahmedshanto <sakibsanto57@gmail.com>
2024-09-22 23:19:12 +0600

    created a firebase project and binded it with the application , installing necessary plugins, and turned on the google authentication on the firebase project

---
+++ lib/utils/app-constant.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class AppConstant {
+  static String appMainName = 'Pastry Paradise';
+  static String appPoweredBy = 'Powered By Team Taranga';
+  static const appMainColor = Color.fromARGB(255, 229, 55, 127);
+  static const appScendoryColor =Color.fromARGB(255, 203, 40, 108);
+  static const appTextColor = Color(0xFFFBF5F4);
+  static const appStatusBarColor = Color(0xFFFBF5F4);
+  static const String whatsApp = "+8801719364706";
+  static const String personalMail = "saimasakar4706@gmail.com";
+  static const String Owner = "Saima Sarkar";
+  static const String fbLink = "https://www.facebook.com/saima.ripon.56";
+  static const String developer ="Sakib Ahmed Shanto";
+  static const String developerFB ="https://www.facebook.com/profile.php?id=61554610147869";
+}
